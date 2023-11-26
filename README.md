154556dfd56a89f03ca7722d780bfd8693d64104
Thomas Burgess <thomasburgess2000@gmail.com>
2023-11-25 21:21:16 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# babylonian
+
+Template for a Bablyon.js TypeScript project that uses Bun.
+
+## Build
+```bun build ./index.ts --outdir ./build --watch```
+
+## Run
+```bun --hot run server.ts```
---
+++ src/game.ts
@@ -0,0 +1,30 @@
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+
+import { ArcRotateCamera, Engine, HemisphericLight, MeshBuilder, Scene, Vector3 } from '@babylonjs/core';
+
+export function startGame() {
+  // Create canvas and engine
+  const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+  const engine = new Engine(canvas, true);
+  window.addEventListener('resize', () => {
+    engine.resize();
+  });
+
+  // Create a basic scene
+  const scene = new Scene(engine);
+  const camera = new ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 15, Vector3.Zero(), scene);
+  camera.attachControl(canvas, true);
+  const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
+
+  // Add a simple sphere
+  MeshBuilder.CreateSphere('sphere', { diameter: 2 }, scene);
+
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
+
+  window.addEventListener('resize', () => {
+    engine.resize();
+  });
+}
